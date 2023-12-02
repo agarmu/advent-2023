9029fddbb2d5dd18ef1f381391e6300cf618b9b3
Mukul Agarwal <agarmukul23@gmail.com>
2023-12-01 23:23:59 -0500

    add day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,47 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(input
+        .lines()
+        .map(|x| {
+            let mut q = x.bytes()
+                .filter(|x| x >= &b'0' && x <= &b'9')
+                .map(|x| (x - b'0') as u32);
+            let fd = q.next().unwrap();
+            let ld = q.last().unwrap_or(fd);
+            fd * 10 + ld
+        }).sum())
+}
+const R: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+pub fn make_stack(input: &str) -> Vec<u32> {
+    let x = input.as_bytes();
+    let mut res: Vec<u32> = vec![];
+    for i in 0..input.len() {
+        for (k, r) in R.iter().enumerate() {
+            if i + r.len() <= input.len() {
+               if input[i..].starts_with(r) {
+                    res.push(k as u32 + 1);
+                    continue;
+               }
+            }
+        }
+        if x[i] >= b'0' && x[i] <= b'9' {
+            res.push((x[i] - b'0') as u32);
+        }
+    }
+    res
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+
+    Some(input
+        .lines()
+        .map(|x| {
+            let res = make_stack(x);
+            res[0] * 10 + res[res.len() - 1]
+        }).sum())
 }
 
 #[cfg(test)]
@@ -15,7 +51,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
