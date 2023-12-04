c78298cfd858d6dea2e058a5fc812fb64699d72b
Mukul Agarwal <agarmukul23@gmail.com>
2023-12-04 00:48:14 -0500

    add optimizations for day 4

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `33.7µs` | `221.0µs` |
-| [Day 2](./src/bin/02.rs) | `48.3µs` | `47.8µs` |
-| [Day 3](./src/bin/03.rs) | `81.8µs` | `75.5µs` |
-| [Day 4](./src/bin/04.rs) | `188.6µs` | `188.9µs` |
+| [Day 1](./src/bin/01.rs) | `37.4µs` | `220.0µs` |
+| [Day 2](./src/bin/02.rs) | `47.9µs` | `47.7µs` |
+| [Day 3](./src/bin/03.rs) | `76.3µs` | `72.8µs` |
+| [Day 4](./src/bin/04.rs) | `105.1µs` | `100.4µs` |
 | [Day 5](./src/bin/05.rs) | `-` | `-` |
 | [Day 6](./src/bin/06.rs) | `-` | `-` |
 | [Day 7](./src/bin/07.rs) | `-` | `-` |
@@ -37,7 +37,7 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 24](./src/bin/24.rs) | `-` | `-` |
 | [Day 25](./src/bin/25.rs) | `-` | `-` |
 
-**Total: 0.89ms**
+**Total: 0.71ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/04.rs
+++ src/bin/04.rs
@@ -7,13 +7,15 @@ pub struct Card {
     winners: u32
 }
 
-
+fn get_count(line: &str)-> usize {
+    let (_, u) = line.split_once(":").unwrap();
+    let (before, after) = u.split_once("|").unwrap();
+    let a = before.split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>();
+    after.split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap()).filter(|x| a.contains(x)).count()
+}
 pub fn part_one(input: &str) -> Option<u32> {
     Some(input.lines().map(|x| {
-        let mut u = x.split(":").skip(1).next().unwrap().split("|");
-        let iter = u.next().unwrap().split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap());
-        let a = HashSet::<_>::from_iter(iter);
-        let c = u.next().unwrap().split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap()).filter(|x| a.contains(x)).count();
+        let c = get_count(x);
         let v = if c > 0 { 1 << (c - 1) } else { 0 };
         v
     }).sum())
@@ -23,10 +25,7 @@ pub fn part_two(input: &str) -> Option<u32> {
     let lines = input.lines().enumerate().collect::<Vec<_>>();
     let mut count  = vec![1u32; lines.len()];
     for (i, x) in lines {
-        let mut u = x.split(":").skip(1).next().unwrap().split("|");
-        let iter = u.next().unwrap().split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap());
-        let a = HashSet::<_>::from_iter(iter);
-        let c = u.next().unwrap().split_ascii_whitespace().map(|x| x.parse::<u32>().unwrap()).filter(|x| a.contains(x)).count();
+        let c = get_count(x);
         // the next several get incremented by value of count[i];
         let amt = count[i];
         for j in 1..=c {
