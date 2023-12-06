2b546d9a8ff6ceb1a486460ec669deb951ed58a2
Mukul Agarwal <agarmukul23@gmail.com>
2023-12-06 00:46:56 -0500

    optimize day 6 using quadratic formula (1.21x faster day 1, 97x faster day 2)

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,5 +1,6 @@
-advent_of_code::solution!(6);
 
+advent_of_code::solution!(6);
+const DELTA: f64 = 0.001;
 pub fn part_one(input: &str) -> Option<u32> {
     let mut lines = input.lines();
     let times = lines
@@ -8,54 +9,52 @@ pub fn part_one(input: &str) -> Option<u32> {
         .strip_prefix("Time:")
         .unwrap()
         .split_ascii_whitespace()
-        .map(|x| x.parse::<u32>().unwrap());
+        .map(|x| x.bytes().fold(0.0f64, |a, x| 10.0 * a + (x - b'0') as f64));
     let distances = lines
         .next()
         .unwrap()
         .strip_prefix("Distance:")
         .unwrap()
         .split_ascii_whitespace()
-        .map(|x| x.parse::<u32>().unwrap());
+        .map(|x| x.bytes().fold(0.0f64, |a, x| 10.0 * a + (x - b'0') as f64));
     let mut res = 1;
     for (time, dist) in times.zip(distances) {
-        let mut q = 0;
-        for accel_time in 1..time {
-            if dist < (time - accel_time) * accel_time {
-                q += 1;
-            }
-        }
-        res *= q;
+        let disc = time * time - 4.0 * dist;
+        let disc = disc.sqrt() / 2.0;
+        let min = (f64::max(0.0 ,time/2.0 - disc) + DELTA).ceil() as u32;
+        let max = (f64::min(time ,time/2.0 + disc) - DELTA).floor() as u32;
+        res *= max - min + 1;
     }
     Some(res)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(input: &str) -> Option<u32> {
     let mut lines = input.lines();
     let time = lines
         .next()
         .unwrap()
         .strip_prefix("Time:")
         .unwrap()
-        .chars()
-        .filter(|x| x.is_digit(10))
-        .collect::<String>();
+        .bytes()
+        .filter(|x| !x.is_ascii_whitespace())
+        .fold(0.0f64, |a, x| 10.0 * a + (x - b'0') as f64);
     let dist = lines
         .next()
         .unwrap()
         .strip_prefix("Distance:")
         .unwrap()
-        .chars()
-        .filter(|x| x.is_digit(10))
-        .collect::<String>();
-    let time = time.parse().unwrap();
-    let dist: u64 = dist.parse().unwrap();
-    let mut q = 0;
-    for accel_time in 1..time {
-        if dist < (time - accel_time) * accel_time {
-            q += 1;
-        }
-    }
-    Some(q)
+        .bytes()
+        .filter(|x| !x.is_ascii_whitespace())
+        .fold(0.0f64, |a, x| 10.0 * a + (x - b'0') as f64);
+    let disc = time * time - 4.0 * dist;
+    if disc <= 0.0 { return None; }
+    let disc = disc.sqrt() / 2.0;
+    let fmin = f64::max(0.0 ,time/2.0 - disc);
+    let fmax = f64::min(time ,time/2.0 + disc);
+    let min = (fmin + DELTA).ceil() as u32;
+    let max = (fmax - DELTA).floor() as u32;
+    if max <= min { return None; }
+    Some(max - min + 1)
 }
 
 #[cfg(test)]
