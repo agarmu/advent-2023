ef55492a736100517d35a377dee773fe1dd67e7a
Mukul Agarwal <agarmukul23@gmail.com>
2023-12-21 18:00:03 +0530

    add day 12 pt 1

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,10 +1,97 @@
+use itertools::Itertools;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator, IntoParallelIterator};
+
 advent_of_code::solution!(12);
 
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Spring {
+    Working, Damaged, Unknown
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+#[derive(Debug, Clone)]
+struct Record {
+    springs: Vec<Spring>,
+    counts :Vec<usize>
+}
+
+impl Record {
+    fn parse<const N: usize>(x: &str) -> Self {
+        let (f, l) = x.split_once(" ").unwrap();
+        let mut springs = f.bytes().map(Spring::parse).collect_vec();
+        let mut counts = l.split(",").filter_map(|x| x.parse().ok()).collect_vec();
+        if N > 1 {
+            springs.push(Spring::Unknown);
+            springs = springs.repeat(5);
+            counts = counts.repeat(5);
+        }
+        Self {springs, counts}
+    }
+
+    fn is_valid(&self) -> bool {
+        self
+            .springs
+            .iter()
+            .group_by(|x| *x)
+            .into_iter()
+            .filter_map(|(x, g)| {
+                if *x == Spring::Damaged { Some(g.count()) } else { None }
+            }).eq(self.counts.iter().copied())
+    }
+
+    fn count_valid_arrangements(mut self) -> u64 {
+        // we add an extra value at the end to make recursion simpler
+        // (since we don't care about working springs at all-- just use that)
+        self.springs.push(Spring::Working);
+        let mut memo = vec![vec![None; self.springs.len()]; self.counts.len()];
+        Record::memoized_counting(&self.springs, &self.counts, &mut memo)
+    }
+    fn memoized_counting(springs: &[Spring], counts: &[usize], memo: &mut [Vec<Option<u64>>]) -> u64 {
+        if counts.is_empty() {
+            if springs.contains(&Spring::Damaged) {
+                0
+            } else {
+                1
+            }
+        } else if springs.len() < counts.iter().sum::<usize>() + counts.len() {
+            return 0;
+        } else if let Some(c) = memo[counts.len() - 1][springs.len() - 1] {
+            c
+        } else {
+            let mut arrangements = 0;
+            if springs[0] != Spring::Damaged {
+                arrangements += Record::memoized_counting(&springs[1..], counts, memo);
+            }
+            let next_grp = counts[0];
+            if !springs[..next_grp].contains(&Spring::Working) && springs[next_grp] != Spring::Damaged {
+                arrangements += Record::memoized_counting(&springs[(next_grp + 1)..], &counts[1..], memo);
+            }
+            memo[counts.len() - 1][springs.len() - 1] = Some(arrangements);
+            arrangements
+        }
+    }
+
+}
+
+impl Spring {
+    fn parse(x: u8) -> Self {
+        match x {
+            b'.' => Self::Working,
+            b'?' => Self::Unknown,
+            b'#' => Self::Damaged,
+            _ => unreachable!()
+        }
+    }
+}
+
+
+pub fn part_one(input: &str) -> Option<u64> {
+    Some(input.lines().map(Record::parse::<1>)
+        .into_iter()
+        .map(|x| Record::count_valid_arrangements(x))
+        .sum())
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
